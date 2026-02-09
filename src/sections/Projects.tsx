6a462aab5c13d9dfa19651753f1a3b389a3ae249
user.email <mohammedsoliman144@gmail.com>
2026-02-09 23:24:06 +0200

    Added Images Inside assets

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,4 +1,7 @@
 import Card from "@/components/Card";
+import aiSass from '@/assets/images/ai-saas.png'
+import socialMedia from '@/assets/images/social-media.png'
+import stopWatch from '@/assets/images/stopwatch.png'
 
 export default function Projects() {
     return <section id='projects' className="bg-primary w-screen py-15 relative">
@@ -6,19 +9,19 @@ export default function Projects() {
             <h2 className="capitalize text-white text-3xl md:text-6xl font-bold mb-15 text-center tracking-wide">my projects</h2>
             <div className="cards grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-between items-center gap-12 ">
                 <Card 
-                    imgSrc="/ai-saas.png" 
+                    imgSrc={aiSass} 
                     imgAlt="AI Saas Platform" 
                     contentTitle="ai saaS platform"
                     contentDescription="A modern SaaS platform built with Next.js and OpenAI  intergration, featuring real-time AI-powered content generation and analytics."
                     items={['Next.js', 'OpenAI', 'TailwindCSS']}/>
                 <Card 
-                    imgSrc="/social-media.png" 
+                    imgSrc={socialMedia} 
                     imgAlt="Social Media Dashboard" 
                     contentTitle="Social Media Dashboard"
                     contentDescription="A comprenhensive social media management dashboard with analytics, scheduling, and engagement tracking features."
                     items={['React', 'Node.js', 'MonogoDB']}/>
                 <Card 
-                    imgSrc="/stopwatch.png" 
+                    imgSrc={stopWatch}
                     imgAlt="Timer App" 
                     contentTitle="productivity timer"
                     contentDescription="A sleek productivity timer application with customizable work sessions, statistics tracking, and dark mode support."
